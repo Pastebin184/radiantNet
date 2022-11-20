2216dd868d290382b2429ee7f0faf3dd090f3ed6
iBlqzed <iBlqzed@users.noreply.github.com>
2022-11-20 15:35:51 -0600

    Updated command system

--- Prisons/Prisons BP/scripts/globalVars.js
+++ Prisons/Prisons BP/scripts/globalVars.js
@@ -1,7 +1,53 @@
 export const config = {
-    commandPrefix: '-'
+    commandPrefix: '-',
+    moneyScoreboard: 'money',
+    xpScoreboard: 'xp'
 }
 
 export const commandCooldowns = {
     spawn: 100
+}
+
+/**@type {{[key: string]: {cost: number, xp: number}}} */
+export const itemStats = {
+    "minecraft:coal": {
+        cost: 1,
+        xp: 1
+    },
+    "minecraft:raw_copper": {
+        cost: 2,
+        xp: 4
+    },
+    "minecraft:raw_iron": {
+        cost: 3,
+        xp: 9
+    },
+    "minecraft:raw_gold": {
+        cost: 4,
+        xp: 16
+    },
+    "minecraft:lapis_lazuli": {
+        cost: 5,
+        xp: 25
+    },
+    "minecraft:redstone": {
+        cost: 6,
+        xp: 36
+    },
+    "minecraft:diamond": {
+        cost: 7,
+        xp: 49
+    },
+    "minecraft:netherrack": {
+        cost: 8,
+        xp: 64
+    },
+    "minecraft:quartz": {
+        cost: 9,
+        xp: 81
+    },
+    "minecraft:netherite_scrap": {
+        cost: 10,
+        xp: 100
+    }
 }
--- Prisons/Prisons BP/scripts/index.js
+++ Prisons/Prisons BP/scripts/index.js
@@ -1,5 +1,5 @@
-import { Client } from "./Api/index.js"
-import { config } from "./globalVars.js"
+import { Client, Item } from "./Api/index.js"
+import { commandCooldowns, config, itemStats } from "./globalVars.js"
 import { checkCommandCooldown } from "./utils.js"
 
 const client = new Client({ command: { enabled: config.commandPrefix } })
@@ -8,6 +8,28 @@ client.on("Chat", (data) => {
     data.cancel()
     client.world.broadcast(`§l§8[§r${data.player.getTags().find(tag => tag.startsWith("rank:")) ? data.player.getTags().filter(tag => tag.startsWith('rank:')).map(tag => tag.slice(5)).join('§l§8] [§r') : "§6Member"}§l§8]§r ${data.player.getName()}: ${data.message}`)
 })
+
+client.on("PlayerJoin", (player) => {
+    const log = player.getLog()
+    for (const name in commandCooldowns) log.set(`${name}Cooldown`, 0)
+})
+
+client.on("Tick", () => {
+    client.world.getAllPlayers().forEach(player => {
+        const log = player.getLog()
+        for (const name in commandCooldowns) {
+            const val = log.get(`${name}Cooldown`)
+            log.set(`${name}Cooldown`, val !== 0 ? val - 1 : 0)
+        }
+    })
+})
+
+// client.on("BlockBreak", (data) => {
+//     const itemId = data.player.getDimension().getEntitiesAtLocation(block.location).filter(entity => entity.getId() === 'minecraft:item')[0]?.getComponent("item")?.itemStack?.typeId
+//     if (!itemId) return
+//     const stats = itemStats[itemId]
+//     if (!stats) return
+// })
 
 client.commands.create({
     name: "spawn",
