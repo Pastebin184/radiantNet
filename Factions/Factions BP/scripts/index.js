96d7d5216833cbec36864d9c548239b134952434
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2023-03-23 23:39:26 -0700

    please dont look at this <3

--- Factions/Factions BP/scripts/index.js
+++ Factions/Factions BP/scripts/index.js
@@ -1,1 +1,9 @@
+import { world } from "@minecraft/server";
+import { system } from "@minecraft/server";
 
+const amongUs = "Gay among us sex "
+console.warn(amongUs * 100)
+
+system.runInterval(() =>{
+    world.sendMessage(amongUs);
+})
