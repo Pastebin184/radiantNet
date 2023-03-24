7e5024bc2c48ae9aa0aeea7204c59fdb3681a998
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2023-03-24 09:29:12 -0700

    Update index.js

--- Factions/Factions BP/scripts/index.js
+++ Factions/Factions BP/scripts/index.js
@@ -1,9 +1,50 @@
 import { world } from "@minecraft/server";
 import { system } from "@minecraft/server";
 
-const amongUs = "Gay among us sex "
+const amongUs = ""
 console.warn(amongUs * 100)
 
-system.runInterval(() =>{
-    world.sendMessage(amongUs);
+
+function isRank(tag) {
+    tag = tag.toString()
+    const rankName = tag.slice(5)
+    if (tag.startsWith('rank_'))
+        return rankName
+    else return false
+}
+
+
+const findRank = (tags) => {
+    let i = 0
+    let x = []
+    while (i < tags.length) {
+        if (tags[i].startsWith('rank_')) {
+            x.push([isRank(`${tags[i]}`)])
+            console.error(x)
+        }
+        i++
+    }
+    console.error(x)
+    return x[1]
+}
+
+
+const isPlayer = (playerName) => !world.getPlayers({ name: playerName }).next().done
+
+system.runInterval(() => {
+
+})
+
+world.events.beforeChat.subscribe((data) => {
+    const player = data.sender
+    const message = data.message
+    data.cancel = true
+    world.sendMessage(`[${findRank(player.getTags())}] ${player.name}`)
+
+
+
+    if (!message.startsWith('-'))
+        return
+    findRank(player.getTags())
+
 })
