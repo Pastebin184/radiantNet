803dccf2e16b05db513b7f3f24269f7fd45583db
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2023-08-19 02:43:11 -0700

    manifest

--- Factions/Factions BP/scripts/index.js
+++ Factions/Factions BP/scripts/index.js
@@ -1,4 +1,27 @@
-import { world } from "@minecraft/server";
-import { system } from "@minecraft/server";
-import { commandCreate } from 'commands.js'
+import { world, system } from "@minecraft/server";
+const prefix = '-'
 
+
+world.beforeEvents.chatSend.subscribe((data) => {
+    let player = data.sender
+    data.cancel = true 
+    function hasRank() {
+        if (player.getTags().filter((tags) => tags.startsWith("rank-"))[0]) {
+            return player.getTags().filter((tags) => tags.startsWith("rank-"))[0].toString().slice(5)
+        }
+        else (!player.getTags().filter((tags) => tags.startsWith("rank-")))
+        {
+            return 'Member'
+        }
+    }
+    if (hasRank()) world.sendMessage(`<${hasRank()}§f> <${player.nameTag}> ${data.message}`) 
+    
+    if (data.message.startsWith(prefix)) {
+        switch (data.message.replace(`${prefix}`, '')){
+            case 'spawn':
+                player.runCommandAsync("tp @s 0 100 0 ")
+            
+    }}
+
+
+})
