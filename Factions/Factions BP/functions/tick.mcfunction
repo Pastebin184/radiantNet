e9bb0953c00ab139e547f886855c8013ea01db15
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2023-08-19 17:58:40 -0700

    release?

--- Factions/Factions BP/functions/tick.mcfunction
+++ Factions/Factions BP/functions/tick.mcfunction
@@ -1,3 +1,7 @@
 function sellpad
 function shop/shopshop
 function shop/shopfill
+scoreboard players add @a money 0
+scoreboard players add @a spawnerPity_1 0
+scoreboard players add @a spawnerPity_2 0
+scoreboard players add @a spawnerPity_3 0
--- Factions/Factions BP/scripts/index.js
+++ Factions/Factions BP/scripts/index.js
@@ -1,4 +1,4 @@
-import { world, system } from "@minecraft/server";
+import { world, system, Vector } from "@minecraft/server";
 const prefix = '-'
 
 
@@ -6,6 +6,7 @@ world.beforeEvents.chatSend.subscribe((data) => {
     let player = data.sender
     data.cancel = true 
     function hasRank() {
+        if (data.message.startsWith(!prefix)) { 
         if (player.getTags().filter((tags) => tags.startsWith("rank-"))[0]) {
             return player.getTags().filter((tags) => tags.startsWith("rank-"))[0].toString().slice(5)
         }
@@ -14,14 +15,34 @@ world.beforeEvents.chatSend.subscribe((data) => {
             return 'Member'
         }
     }
+    }
     if (hasRank()) world.sendMessage(`<${hasRank()}§f> <${player.nameTag}> ${data.message}`) 
     
     if (data.message.startsWith(prefix)) {
+        let sp = player.getSpawnPoint()
         switch (data.message.replace(`${prefix}`, '')){
             case 'spawn':
-                player.runCommandAsync("tp @s 0 100 0 ")
-            
-    }}
+                player.runCommandAsync("tp @s 146 153 101")
+                player.sendMessage("Teleported to spawn!")
+                break
+            case 'sethome':
+                system.run(() =>
+                {
+                    sp = player.getSpawnPoint()
+                    player.runCommandAsync("spawnpoint @s ~ ~ ~")
+                    player.sendMessage(`Home set)`)
+                })
+                break
+            case 'home':
+                system.run(() =>
+                {
+                    player.teleport(new Vector(sp.x,sp.y,sp.z))
+                })
+                break
+            default:
+                player.sendMessage("Not a command")
+                break
+            }} 
 
 
 })
